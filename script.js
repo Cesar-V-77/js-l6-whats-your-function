ac5486ccaaaadd24ef9425f7c26145bd36fabb05
cesar_v <indigo.peacewaters@gmail.com>
2026-01-17 19:50:24 -0600

    Write a function with 1 parameter

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+//Write a Function
 var startMyDay = function () {
   return `It's time for coffee and breakfast!`;
 };
 
 console.log(startMyDay());
+
+//Write a Function with 1 Parameter
+var favoriteCookie = function (cookie) {
+  return `My favorite cookie is ${cookie}.`;
+};
+
+console.log(favoriteCookie("chocolate chip"));
